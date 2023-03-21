3c5cb1dedc90934b0becec50e3e7df66c0659e9b
Conner-Berg <connerwberg@gmail.com>
2023-03-21 13:36:57 -0400

    Move .info to proto, add myLibrary

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
 	this.read = read;
-	this.info = function () {
-		let logRead;
-		read ? (logRead = "read") : (logRead = "not read yet");
-		console.log(
-			title + " by " + author + ", " + pages + " pages, " + logRead
-		);
-	};
 }
+
+Book.prototype.info = function () {
+	let logRead;
+	this.read ? (logRead = "read") : (logRead = "not read yet");
+	console.log(
+		this.title +
+			" by " +
+			this.author +
+			", " +
+			this.pages +
+			" pages, " +
+			logRead
+	);
+};
+
+function addBookToLibrary() {}
